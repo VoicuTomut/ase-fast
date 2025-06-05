67623788d9e257048e51f774aae2697c6b6ea04f
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2025-06-05 08:37:37 +0200

    Annotate

--- ase/calculators/vasp/create_input.py
+++ ase/calculators/vasp/create_input.py
@@ -1555,18 +1555,18 @@ class GenerateVaspInput:
         self.converged = None
         self.setups_changed = None
 
-    def default_nelect_from_ppp(self):
+    def default_nelect_from_ppp(self) -> float:
         """ Get default number of electrons from ppp_list and symbol_count
 
         "Default" here means that the resulting cell would be neutral.
         """
-        symbol_valences = []
+        symbol_valences: list[tuple[str, float]] = []
         for filename in self.ppp_list:
             with open_potcar(filename=filename) as ppp_file:
                 r = read_potcar_numbers_of_electrons(ppp_file)
                 symbol_valences.extend(r)
         assert len(self.symbol_count) == len(symbol_valences)
-        default_nelect = 0
+        default_nelect = 0.0
         for ((symbol1, count),
              (symbol2, valence)) in zip(self.symbol_count, symbol_valences):
             assert symbol1 == symbol2
