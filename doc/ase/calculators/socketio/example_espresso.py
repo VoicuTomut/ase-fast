fc5eb718518f03508ad29150d64732837300b97a
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-07-22 13:33:34 +0200

    update the quantum espresso socket io example

--- doc/ase/calculators/socketio/example_espresso.py
+++ doc/ase/calculators/socketio/example_espresso.py
@@ -8,41 +8,28 @@ from ase.optimize import BFGS
 atoms = molecule('H2O', vacuum=3.0)
 atoms.rattle(stdev=0.1)
 
-# Environment-dependent parameters (please configure before running):
-pseudopotentials = {'H': 'H.pbe-rrkjus.UPF', 'O': 'O.pbe-rrkjus.UPF'}
-pseudo_dir = '.'
+# Environment-dependent parameters (please configure before running).
+# Use any files you have, but these are available in the ase-datafiles project.
+pseudopotentials = {'H': 'h_lda_v1.4.uspp.F.UPF', 'O': 'o_lda_v1.2.uspp.F.UPF'}
 
 # In this example we use a UNIX socket.  See other examples for INET socket.
 # UNIX sockets are faster then INET sockets, but cannot run over a network.
 # UNIX sockets are files.  The actual path will become /tmp/ipi_ase_espresso.
 unixsocket = 'ase_espresso'
 
-# Configure pw.x command for UNIX or INET.
-#
-# UNIX: --ipi {unixsocket}:UNIX
-# INET: --ipi {host}:{port}
-#
 # See also QE documentation, e.g.:
 #
 #    https://www.quantum-espresso.org/Doc/pw_user_guide/node13.html
-#
-command = 'pw.x < PREFIX.pwi --ipi {unixsocket}:UNIX > PREFIX.pwo'.format(
-    unixsocket=unixsocket
-)
 
 espresso = Espresso(
-    command=command,
+    directory='calc-espresso',
     ecutwfc=30.0,
     pseudopotentials=pseudopotentials,
-    pseudo_dir=pseudo_dir,
 )
 
 opt = BFGS(atoms, trajectory='opt.traj', logfile='opt.log')
 
-with SocketIOCalculator(
-    espresso, log=sys.stdout, unixsocket=unixsocket
-) as calc:
-    atoms.calc = calc
+with espresso.socketio(unixsocket=unixsocket) as atoms.calc:
     opt.run(fmax=0.05)
 
 # Note: QE does not generally quit cleanly - expect nonzero exit codes.
