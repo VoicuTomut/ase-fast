1960199e3cc752931b7502ed78c516284b1473a8
Pavel Ondračka <pavel.ondracka@gmail.com>
2025-09-04 18:03:02 +0200

    lammpsrun: don't explicitly set fix nve
    Doing this silently is be highly unexpected. So just set it in case
    user does not provide any explicit fix.

--- ase/calculators/lammps/inputwriter.py
+++ ase/calculators/lammps/inputwriter.py
@@ -215,13 +215,15 @@ def write_lammps_in(lammps_in, parameters, atoms, prismobj,
             + "\n"
         )
 
-    fileobj.write("\n### run\n" "fix fix_nve all nve\n")
+    fileobj.write("\n### run\n")
 
     if "fix" in parameters:
         fileobj.write(
             "\n".join([f"fix {p}" for p in parameters["fix"]])
             + "\n"
         )
+    else:
+        fileobj.write("fix fix_nve all nve\n")
 
     fileobj.write(
         "dump dump_all all custom {1} {0} id type x y z vx vy vz "
