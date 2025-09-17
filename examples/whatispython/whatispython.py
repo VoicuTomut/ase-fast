ef3e1d414181e0db94c60909a7fbcb2c8b5ae4ff
Aaron Flötotto <aaron.floetotto@tu-ilmenau.de>
2025-09-17 16:00:31 +0200

    fixed issues found by ruff

--- examples/whatispython/whatispython.py
+++ examples/whatispython/whatispython.py
@@ -6,7 +6,8 @@ What is Python?
 This section will give a very brief introduction to the Python language.
 
 .. tip::
-   See also the `Python home page <https://www.python.org/>`_ for further information.
+   See also the `Python home page <https://www.python.org/>`_ for further
+   information.
 
 Executing Python code
 =====================
@@ -19,9 +20,13 @@ print("hello")
 
 
 # %%
-# You can also put the ``print("hello")`` line in a file (``hello.py``) and execute it as a Python script with ``python3 hello.py`` or ``python3 -i hello.py`` to enter interactive mode after the file is executed.
+# You can also put the ``print("hello")`` line in a file (``hello.py``) and
+# execute it as a Python script with ``python3 hello.py`` or ``python3 -i
+# hello.py`` to enter interactive mode after the file is executed.
 #
-# Finally, you can put ``#!/usr/bin/env python3`` in the first line of the ``hello.py`` file, make it executable (``chmod +x hello.py``) and execute it like any other executable.
+# Finally, you can put ``#!/usr/bin/env python3`` in the first line of the
+# ``hello.py`` file, make it executable (``chmod +x hello.py``) and execute it
+# like any other executable.
 #
 #
 #
@@ -71,7 +76,9 @@ print("the whole dictionary:", d)
 print("one entry of the dictionary:", d["p"])
 
 # %%
-# In this example all keys are strings and all values are integers. Types can be freely mixed in the same dictionary; any type can be used as a value and most types can be used as keys (mutable objects cannot be keys).
+# In this example all keys are strings and all values are integers. Types can
+# be freely mixed in the same dictionary; any type can be used as a value and
+# most types can be used as keys (mutable objects cannot be keys).
 #
 # A ``list`` object is an ordered collection of arbitrary objects:
 
@@ -82,12 +89,19 @@ print("one list element:", l[1])
 print("negative index:", l[-2])
 
 # %%
-# Indexing a list with negative numbers counts from the end of the list, so element ``-2`` is the second last.
+# Indexing a list with negative numbers counts from the end of the list, so
+# element ``-2`` is the second last.
 #
-# A tuple behaves like a list -- except that it can’t be modified in place. Objects of types list and dict are mutable -- all the other types listed in the table are immutable, which means that once an object has been created, it can not change. Tuples can therefore be used as dictionary keys, lists cannot.
+# A tuple behaves like a list -- except that it can’t be modified in place.
+# Objects of types list and dict are mutable -- all the other types listed in
+# the table are immutable, which means that once an object has been created, it
+# can not change. Tuples can therefore be used as dictionary keys, lists
+# cannot.
 #
 # .. note::
-#    List and dictionary objects can change. Variables in Python are references to objects -- think of the ``=`` operator as a “naming operator”, not as an assignment operator. This is demonstrated here:
+#    List and dictionary objects can change. Variables in Python are references
+#    to objects -- think of the ``=`` operator as a “naming operator”, not as
+#    an assignment operator. This is demonstrated here:
 
 # %%
 a = ["q", "w"]
@@ -97,9 +111,13 @@ print("the original, changed list:", a)
 print("the second list:", b)
 
 # %%
-# The line ``b = a`` gives a new name to the array, and both names now refer to the same list.
+# The line ``b = a`` gives a new name to the array, and both names now refer to
+# the same list.
 #
-# However, often a new object is created and named at the same time, in this example the number ``42`` is not modified, a new number ``47`` is created and given the name ``d``. And later, ``e`` is a name for the number ``47``, but then a new number ``48`` is created, and ``e`` now refers to that number:
+# However, often a new object is created and named at the same time, in this
+# example the number ``42`` is not modified, a new number ``47`` is created and
+# given the name ``d``. And later, ``e`` is a name for the number ``47``, but
+# then a new number ``48`` is created, and ``e`` now refers to that number:
 
 # %%
 c = 42
@@ -113,7 +131,9 @@ print("second and third number:", (d, e))
 
 # %%
 # .. note::
-#    Another very important type is the ``ndarray`` type described here: `Numeric arrays in Python <https://ase-lib.org/numpy.html#numpy>`_. It is an array type for efficient numerics, and is heavily used in ASE.
+#    Another very important type is the ``ndarray`` type described here:
+#    `Numeric arrays in Python <https://ase-lib.org/numpy.html#numpy>`_. It is
+#    an array type for efficient numerics, and is heavily used in ASE.
 #
 # Loops
 # =====
@@ -125,7 +145,9 @@ for x in things:
     print(x)
 
 # %%
-# The ``things`` object could be any sequence. Strings, tuples, lists, dictionaries, ndarrays and files are sequences. Try looping over some of these types.
+# The ``things`` object could be any sequence. Strings, tuples, lists,
+# dictionaries, ndarrays and files are sequences. Try looping over some of
+# these types.
 #
 # Often you need to loop over a range of numbers:
 
@@ -150,7 +172,8 @@ print(f(5))
 print(f(5, n=8))
 
 # %%
-# Here ``f`` is a function, ``x`` is an argument, ``m`` and ``n`` are keywords with default values ``2`` and ``1`` and ``y`` is a variable.
+# Here ``f`` is a function, ``x`` is an argument, ``m`` and ``n`` are keywords
+# with default values ``2`` and ``1`` and ``y`` is a variable.
 #
 # A class is defined like this:
 
@@ -168,9 +191,12 @@ class A:
 
 
 # %%
-# You can think of a class as a template for creating user defined objects. The ``__init__()`` function is called a constructor, it is being called when objects of this type are being created.
+# You can think of a class as a template for creating user defined objects. The
+# ``__init__()`` function is called a constructor, it is being called when
+# objects of this type are being created.
 #
-# In the class ``A`` ``__init__`` is a constructor, ``c`` is an attribute and ``m`` and ``get_c`` are methods.
+# In the class ``A`` ``__init__`` is a constructor, ``c`` is an attribute and
+# ``m`` and ``get_c`` are methods.
 
 # %%
 a = A(7)
@@ -184,25 +210,24 @@ print(a.m(3))
 # Importing modules
 # =================
 #
-# There are several ways to import modules (either ``.py`` files in your working directoryy or packages that are installed in your python environment):
-
-# %%
-import numpy
-
-numpy.linspace(1, 10, num=5)
-
-# %%
-# or (in this case the standard method):
-
-# %%
-import numpy as np
-
-np.linspace(1, 10, num=5)
-
-# %%
-# or:
-
-# %%
-from numpy import linspace
-
-linspace(1, 10, num=5)
+# There are several ways to import modules (either ``.py`` files in your
+# working directory or packages that are installed in your python environment).
+# For example:
+#
+# ::
+#
+#   import numpy; numpy.linspace(1, 10, num=5)
+#
+# and
+#
+# ::
+#
+#   import numpy as np; np.linspace(1, 10, num=5)
+#
+# and
+#
+# ::
+#
+#   from numpy import linspace; linspace(1, 10, num=5)
+#
+# all yield the same result.
