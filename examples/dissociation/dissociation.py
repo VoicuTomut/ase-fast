a591848d91dcffdd94ce14e5b3b08f485755f3a5
Zausinator <zausi.bergmann@gmail.com>
2025-09-19 19:19:14 +0000

    Applied snake_case suggestions to conf.py
    Co-authored by: @ajjackson

--- examples/dissociation/dissociation.py
+++ examples/dissociation/dissociation.py
@@ -37,7 +37,7 @@ slab.set_pbc((1, 1, 0))
 
 # Add the N2 Molecule,  oriented at 60 degrees:
 d = 1.10  # N2 bond length
-N2mol = Atoms(
+n2_mol = Atoms(
     'N2', positions=[[0.0, 0.0, 0.0], [0.5 * 3**0.5 * d, 0.5 * d, 0.0]]
 )
 
@@ -45,7 +45,7 @@ N2mol = Atoms(
 # Then we put the adsorbate onto the surface
 #
 
-add_adsorbate(slab, N2mol, height=1.0, position='fcc')
+add_adsorbate(slab, n2_mol, height=1.0, position='fcc')
 
 # %%
 # And add a calculator for the forces and energies:
@@ -118,15 +118,15 @@ relax.run()
 # Finally, we compare intermediate steps to the initial energy
 
 energy_initial = initial.get_potential_energy()
-N2_distances = []
+n2_distances = []
 energy_differences = []
 for i, config in enumerate(configs):
-    N2_distance = np.linalg.norm(config[-2].position - config[-1].position)
+    n2_distance = np.linalg.norm(config[-2].position - config[-1].position)
     delta_energy = config.get_potential_energy() - energy_initial
-    N2_distances.append(N2_distance)
+    n2_distances.append(n2_distance)
     energy_differences.append(delta_energy)
     print(
-        f'{i:>3}\td = {N2_distance:>4.3f}AA '
+        f'{i:>3}\td = {n2_distance:>4.3f}AA '
         f'\tdelta energy = {delta_energy:>5.3f}eV'
     )
 
@@ -144,8 +144,8 @@ fig, axs = plt.subplots(
     gridspec_kw={'height_ratios': [0.5, 1.0]},
 )
 
-axs[0].plot(N2_distances, energy_differences)
-scat = axs[0].scatter(N2_distances[0], energy_differences[0], s=10.0**2.0)
+axs[0].plot(n2_distances, energy_differences)
+scat = axs[0].scatter(n2_distances[0], energy_differences[0], s=10.0**2.0)
 axs[0].set_ylabel(r'$E(i) - E_{\mathrm{initial}}$ (meV)')
 axs[0].set_xlabel(r'$d_{\mathrm{N}-\mathrm{N}} (\AA)$')
 
@@ -157,7 +157,7 @@ axs[1].set_xlim(0.0, 14.833)
 
 
 def animate(i):
-    scat.set_offsets((N2_distances[i], energy_differences[i]))
+    scat.set_offsets((n2_distances[i], energy_differences[i]))
 
     # Remove the previous atomic plot
     [p.remove() for p in axs[1].patches]
