implement get_gradient() again this time actually with the correct sign

ase/test/optimize/test_pure.py
@@ -32,11 +32,10 @@ class BoothFunctionOptimizable(Optimizable):
         a, b = self.ab(*self.xy)
         return a * a + b * b
 
-    def get_minus_gradient(self):
+    def get_gradient(self):
         x, y = self.xy
         a, b = self.ab(*self.xy)
-        # XXX negative gradient
-        return -np.array([2 * a + 4 * b, 4 * a + 2 * b])
+        return np.array([2 * a + 4 * b, 4 * a + 2 * b])
 
     def iterimages(self):
         return iter([])