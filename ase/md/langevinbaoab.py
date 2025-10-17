351075f80143634b6e2bfc20941772eeb7a79fb0
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2025-10-17 10:07:40 -0400

    Significant refactoring of LangevinBAOAB constructor, and a few typos in comments

--- ase/md/langevinbaoab.py
+++ ase/md/langevinbaoab.py
@@ -7,7 +7,7 @@ J. Chem. Phys. 138 174102 (2013).
 https://doi.org/10.1063/1.4802990
 
 There is some evidence that other time integration schemes, e.g. BAOA,
-maybe be better (https://pubs.acs.org/doi/full/10.1021/acs.jctc.2c00585),
+may be better (https://pubs.acs.org/doi/full/10.1021/acs.jctc.2c00585),
 and it may be straightforward to add these, but none are not currently
 supported .
 
@@ -18,7 +18,7 @@ https://doi.org/10.1063/1.1755657
 Parameters
 ----------
 atoms: Atoms
-    Atoms object for dynaics.
+    Atoms object for dynamics.
 timestep: float
     Timestep (ASE native units) for time propagation.
 temperature_K: float, optional
@@ -32,6 +32,9 @@ externalstress: float, ndarray(3), narray(6), narray((3, 3)), optional
     compression. Note also that barostat will keep mean stress _including
     kinetic (i.e. ideal gas) contribution_ equal to this value.  Only scalars
     are allowed if `hydrostatic` is True.
+hydrostatic: bool, default False
+    Allow only hydrostaic strain (i.e. preserve cell _shape_ but allow overall
+    scaling of volume).
 T_tau: float, optional
     Time constant for position degree of freedom Langevin. Defaults to 50 *
     `timestep` if not specified.
@@ -41,7 +44,7 @@ P_tau: float, optional
     Langevin timescale for NPT). Defaults to 20 * `T_tau` if T_tau is provided,
     otherwise 1000 * `timestep`.
 P_mass: float, optional
-    Mass used for cell volume dynamics. Default is a heuristic value that aims
+    Mass used for variable cell dynamics. Default is a heuristic value that aims
     for fluctuation period of `P_tau / 4`.
 P_mass_factor: float, default 1.0
     Factor to multiply heuristic `P_mass`.
@@ -50,11 +53,9 @@ disable_cell_langevin: bool, default False
     not `None`.  Variable cell will still be done if `externalstress` is not
     `None`, in which case cell equilibration will rely on interaction between
     cell and position DOFs.
-hydrostatic: bool, default False
-    Allow only hydrostaic strain (i.e. preserve cell _shape_ but allow overall
-    scaling of volume).
-initial_nsteps: int, default 0
-    Initial nsteps to set (for sensible output in continuations).
+rng: np.random.Generator or argument to np.random.default_rng, default None
+    Random number generator to use for Langevin random force, required if
+    Langevin is enabled, or integer to be used as seed for new Generator.
 **kwargs: dict
     Additional ase.md.md.MolecularDynamics kwargs.
 """
@@ -77,32 +78,28 @@ class LangevinBAOAB(MolecularDynamics):
         *,
         temperature_K=None,
         externalstress=None,
+        hydrostatic=False,
         T_tau=None,
         P_tau=None,
         P_mass=None,
-        P_mass_factor=None,
+        P_mass_factor=1.0,
         disable_cell_langevin=False,
         rng=None,
-        hydrostatic=False,
-        initial_nsteps=0,
         **kwargs,
     ):
         MolecularDynamics.__init__(self, atoms, timestep, **kwargs)
 
-        self.externalstress = externalstress
-        self.P_mass = P_mass
-        self.P_mass_factor = P_mass_factor if P_mass_factor is not None else 1.0
+        self._set_externalstress_hydrostatic(externalstress, hydrostatic)
         self.disable_cell_langevin = disable_cell_langevin
-        self.rng = rng
-
-        self.nsteps = initial_nsteps
+        self.rng = (rng if isinstance(rng, np.random.Generator)
+                        else np.random.default_rng(rng))
 
         if temperature_K is not None:
             # run constant T, need rng and T_tau
-            if self.rng is None:
+            if not isinstance(self.rng, np.random.Generator):
                 raise RuntimeError(
-                    'Fixed temperature requires `rng` for Langevin, '
-                    f"got '{rng}'"
+                    'Fixed temperature requires np.random.Generator `rng` for '
+                    f'Langevin, got "{rng}"'
                 )
             if T_tau is None:
                 T_tau = 50.0 * self.dt
@@ -114,65 +111,21 @@ class LangevinBAOAB(MolecularDynamics):
         if self.T_tau is not None and self.T_tau <= 0:
             raise ValueError(f'Invalid T_tau {self.T_tau} <= 0')
 
-        self.hydrostatic = None
-        if self.externalstress is not None:
-            self.hydrostatic = hydrostatic
-
-            # promote to ndarray to simplify code below
-            self.externalstress = np.asarray(self.externalstress).reshape((-1))
-            s = self.externalstress.shape
-
-            # run NPH or NPT, need P_mass, and optionally rng and temperature_K
-            if hydrostatic:
-                # external stress must be scalar
-                if s != (1,):
-                    raise ValueError(
-                        'externalstress must be scalar when hydrostatic, '
-                        f"got '{self.externalstress}' with shape "
-                        f'{self.externalstress.shape}'
-                    )
-                self.externalstress = self.externalstress[0]
-            else:
-                # external stress must end up as 3x3 matrix
-                if s == (1,):
-                    self.externalstress = self.externalstress * np.identity(3)
-                elif s == (3,):
-                    self.externalstress = np.diag(self.externalstress)
-                elif s == (6,):
-                    self.externalstress = voigt_6_to_full_3x3_stress(
-                        self.externalstress
-                    )
-                elif s != (3, 3):
-                    raise ValueError(
-                        'externalstress must be scalar, 3-vector (diagonal), '
-                        '6-vector (Voigt), or 3x3 matrix, '
-                        f"'{self.externalstress}' with shape "
-                        f'{self.externalstress.shape}'
-                    )
-
-            if P_tau is None:
-                if self.T_tau is not None:
-                    P_tau = 20.0 * self.T_tau
-                    warnings.warn(
-                        'Got `externalstresse but missing `P_tau`, got '
-                        f'`T_tau`, defaulting to 20 * `T_tau` = {P_tau}'
-                    )
-                else:
-                    P_tau = 1000.0 * self.dt
-                    warnings.warn(
-                        'Got `externalstress` but missing `P_taue and '
-                        f'`T_tau`, defaulting to 1000 * `timestep` = {P_tau}'
-                    )
-        self.P_tau = P_tau
-        if self.P_tau is not None and self.P_tau <= 0:
-            raise ValueError(f'Invalid P_tau {self.P_tau} <= 0')
-
         # default contribution to effective gamma used in _BAOAB_OU that comes
         # from barostat from 2nd term in RHS of Quigley Eq. (5b)
         self.gamma_mod = 0.0
 
         if self.externalstress is not None:
+            # set various quantities required for variable cell dynamics
+
+            # pressure timescale
+            self._set_P_tau(P_tau)
+            if self.P_tau is not None and self.P_tau <= 0:
+                raise ValueError(f'Invalid P_tau {self.P_tau} <= 0')
+
+            # initial momentum of cell DOFs
             self.p_eps = 0.0
+
             # Hope that ASE Atoms.get_number_of_degrees_of_freedom() gives
             # correct value.  It's not, for example, completely obvious what
             # should be # done about the 3 overall translation DOFs, since
@@ -187,55 +140,151 @@ class LangevinBAOAB(MolecularDynamics):
                 )
             self.Ndof = self.atoms.get_number_of_degrees_of_freedom()
 
-            if self.P_mass is None:
-                # set P_mass with heuristic
-                #
-                # originally tried expression from Quigley Eq. 17
-                #    W = 3 N k_B T / (2 pi / tau)^2
-                # empirically didn't work at all
-                #
-                # instead, using empirical value based on tests
-                # of various P_mass, supercell sizes
-                #
-                # supercell of 4 atom Al FCC cell
-                # VARY P_mass: looks like tau \prop sqrt(P_mass)
-                # sc 3 P_mass 1000  T 300 period 34.01360544217687
-                # sc 3 P_mass 10000 T 300 period 91.74311926605505
-                # VARY sc: looks like tau \prop 1/sqrt(N^1/3)
-                # sc 2 P_mass 10000.0 T 400 period 104.16666666666667 (32 atoms)
-                # sc 3 P_mass 10000.0 T 400 period 92.5925925925926  (108 atoms)
-                # sc 4 P_mass 10000.0 T 400 period 66.66666666666667 (256 atoms)
-                # tau = C * sqrt(P_mass) / N**(1/6)
-                # 66 fs for N = 4 * 4^3 = 256, P_mass = 10^4
-                # 66 fs = C * 10000**0.5 / 256.0**(1.0/6.0)
-                # C = 66 fs / (10000**0.5 / 256.0**(1.0/6.0))
-                # C = 1.6630957858612323 fs
-                # P_mass = ((tau / C) * N**(1/6)) ** 2
-                #
-                # note that constant here may be very system (bulk modulus?)
-                # dependent
-                if not self.P_tau > 0:
-                    raise ValueError(
-                        'Heuristic used for P_mass requires P_tau > 0'
-                    )
-                C = 1.66 * units.fs
-                self.barostat_mass_use = (
-                    ((self.P_tau / 4.0) / C) * (len(self.atoms) ** (1.0 / 6.0))
-                ) ** 2
-                warnings.warn(
-                    'Using heuristic P_mass {self.barostat_mass_use} '
-                    f'from P_tau {self.P_tau}'
-                )
-                self.barostat_mass_use *= self.P_mass_factor
-            else:
-                self.barostat_mass_use = self.P_mass
+            self._set_barostat_mass(P_mass, P_mass_factor)
 
+        # must call _after_ var cell quantities are set, because actual 
+        # parameters used in dynamics depend on temperature, so this function
+        # needs to know P_tau, etc
         self.set_temperature(temperature_K, from_init=True)
 
         # initialize forces and stresses
         self._update_accel()
         if self.externalstress is not None:
             self._update_force_eps()
+
+
+    def _set_externalstress_hydrostatic(self, externalstress, hydrostatic):
+        """Set self.externalstress of correct shape and self.hydrostatic
+
+        Parameters
+        ----------
+        externalstress: float or None
+            external stress
+        hydrostatic: bool
+            hydrostatic (fixed shape, variable volume) variations
+        """
+        self.hydrostatic = hydrostatic
+
+        if externalstress is None:
+            self.externalstress = externalstress
+            return None
+
+        # promote to ndarray to simplify code below
+        externalstress = np.asarray(externalstress)
+        if len(externalstress.shape) == 0:
+            externalstress = externalstress.reshape((-1))
+        stress_shape = externalstress.shape
+
+        # reshape to scalar (iff hydrostatic) or 3x3 matrix (general var cell)
+        if self.hydrostatic:
+            # external stress must be scalar
+            if stress_shape != (1,):
+                raise ValueError(
+                    'externalstress must be scalar when hydrostatic, '
+                    f"got '{externalstress}' with shape "
+                    f'{externalstress.shape}'
+                )
+            externalstress = externalstress[0]
+        else:
+            # external stress must end up as 3x3 matrix
+            if stress_shape == (1,):
+                externalstress = externalstress * np.identity(3)
+            elif stress_shape == (3,):
+                externalstress = np.diag(externalstress)
+            elif stress_shape == (6,):
+                externalstress = voigt_6_to_full_3x3_stress(
+                    externalstress
+                )
+            elif stress_shape != (3, 3):
+                raise ValueError(
+                    'externalstress must be scalar, 3-vector (diagonal), '
+                    '6-vector (Voigt), or 3x3 matrix, '
+                    f'got "{externalstress}" with shape '
+                    f'{externalstress.shape}'
+                )
+        self.externalstress = externalstress
+
+
+    def _set_P_tau(self, P_tau):
+        """Set self.P_tau from value, or default based on self.T_tau or self.dt
+
+        Parameters
+        ----------
+        P_tau: float or None
+            time scale for pressure fluctuations
+        """
+        if P_tau is None:
+            if self.T_tau is not None:
+                P_tau = 20.0 * self.T_tau
+                warnings.warn(
+                    'Got `externalstresse but missing `P_tau`, got '
+                    f'`T_tau`, defaulting to 20 * `T_tau` = {P_tau}'
+                )
+            else:
+                P_tau = 1000.0 * self.dt
+                warnings.warn(
+                    'Got `externalstress` but missing `P_tau and '
+                    f'`T_tau`, defaulting to 1000 * `timestep` = {P_tau}'
+                )
+        self.P_tau = P_tau
+
+
+    def _set_barostat_mass(self, P_mass, P_mass_factor):
+        """Set self.barostat_mass based on P_mass and P_mass_factor, as well as
+        variable cell timescale self.P_tau
+
+        Parameters
+        ----------
+        P_mass: float or None
+            barostat mass
+        P_mass_factor: float, default 1.0
+            factor to be applied relative to default value of heuristic
+        """
+        if P_mass is None:
+            # set P_mass with heuristic
+            #
+            # originally tried expression from Quigley Eq. 17
+            #    W = 3 N k_B T / (2 pi / tau)^2
+            # empirically didn't work at all
+            #
+            # instead, using empirical value based on tests
+            # of various P_mass, supercell sizes
+            #
+            # supercell of 4 atom Al FCC cell
+            # VARY P_mass: looks like tau \prop sqrt(P_mass)
+            # sc 3 P_mass 1000  T 300 period 34.01360544217687
+            # sc 3 P_mass 10000 T 300 period 91.74311926605505
+            # VARY sc: looks like tau \prop 1/sqrt(N^1/3)
+            # sc 2 P_mass 10000.0 T 400 period 104.16666666666667 (32 atoms)
+            # sc 3 P_mass 10000.0 T 400 period 92.5925925925926  (108 atoms)
+            # sc 4 P_mass 10000.0 T 400 period 66.66666666666667 (256 atoms)
+            # tau = C * sqrt(P_mass) / N**(1/6)
+            # 66 fs for N = 4 * 4^3 = 256, P_mass = 10^4
+            # 66 fs = C * 10000**0.5 / 256.0**(1.0/6.0)
+            # C = 66 fs / (10000**0.5 / 256.0**(1.0/6.0))
+            # C = 1.6630957858612323 fs
+            # P_mass = ((tau / C) * N**(1/6)) ** 2
+            #
+            # note that constant here may be very system (bulk modulus?)
+            # dependent
+            if not self.P_tau > 0:
+                raise ValueError(
+                    'Heuristic used for P_mass requires P_tau > 0'
+                )
+            C = 1.66 * units.fs
+            barostat_mass = (
+                ((self.P_tau / 4.0) / C) * (len(self.atoms) ** (1.0 / 6.0))
+            ) ** 2
+            warnings.warn(
+                'Using heuristic P_mass {barostat_mass} '
+                f'from P_tau {self.P_tau}'
+            )
+            barostat_mass *= P_mass_factor
+        else:
+            barostat_mass = P_mass
+
+        self.barostat_mass = barostat_mass
+
 
     def set_temperature(self, temperature_K, from_init=False):
         """Set the internal parameters that depend on temperature
@@ -280,7 +329,7 @@ class LangevinBAOAB(MolecularDynamics):
             self._barostat_BAOAB_prefactor = (
                 (sigma / np.sqrt(2.0 * self.barostat_gamma))
                 * np.sqrt(1.0 - np.exp(-2.0 * self.barostat_gamma * self.dt))
-                * np.sqrt(self.barostat_mass_use)
+                * np.sqrt(self.barostat_mass)
             )
             # _does_ include sqrt(mass) factor
 
@@ -332,13 +381,13 @@ class LangevinBAOAB(MolecularDynamics):
         if self.hydrostatic:
             volume = self.atoms.get_volume()
             new_volume = volume * np.exp(
-                0.5 * self.dt * 3.0 * self.p_eps / self.barostat_mass_use
+                0.5 * self.dt * 3.0 * self.p_eps / self.barostat_mass
             )
             new_cell = self.atoms.cell * (new_volume / volume) ** (1.0 / 3.0)
         else:
             new_cell = (
                 self.atoms.cell
-                @ expm(0.5 * self.dt * self.p_eps / self.barostat_mass_use).T
+                @ expm(0.5 * self.dt * self.p_eps / self.barostat_mass).T
             )
 
         self.atoms.set_cell(new_cell, True)
@@ -402,11 +451,11 @@ class LangevinBAOAB(MolecularDynamics):
         velocities due to barostat velocity
         """
         if self.hydrostatic:
-            return (1.0 + 3.0 / self.Ndof) * self.p_eps / self.barostat_mass_use
+            return (1.0 + 3.0 / self.Ndof) * self.p_eps / self.barostat_mass
         else:
             return (
                 self.p_eps + (1.0 / self.Ndof) * np.trace(self.p_eps)
-            ) / self.barostat_mass_use
+            ) / self.barostat_mass
 
     def step(self):
         """Do a time step"""
@@ -435,5 +484,3 @@ class LangevinBAOAB(MolecularDynamics):
         self._BAOAB_B()  # half step vel
         if self.externalstress is not None:
             self._barostat_BAOAB_B()
-
-        # self.nsteps += 1
