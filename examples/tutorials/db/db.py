b0423adddbf5c0926416b2424ea2ddbc8f0834f6
elisabeth.keller <elisabeth.keller.jun@web.de>
2025-10-24 13:44:31 +0200

    Ruff

--- examples/tutorials/db/db.py
+++ examples/tutorials/db/db.py
@@ -16,7 +16,7 @@ with 1, 2 and 3 layers and we will use database files to store the results.
 
 # %%
 # sphinx_gallery_start_ignore
-__import__("run")
+__import__('run')
 # sphinx_gallery_end_ignore
 
 # %%
