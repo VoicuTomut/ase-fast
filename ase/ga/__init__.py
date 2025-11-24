d113a8403a8ed4136636d35f6aafc893e8d95e98
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-11-24 13:58:10 +0100

    override getattr for ase.ga as as to import things from new ase-ga project.

--- ase/ga/__init__.py
+++ ase/ga/__init__.py
@@ -6,21 +6,10 @@ Shorthand for setting and getting
 of an atoms object.
 """
 
+def __getattr__(name):
+    import ase_ga
 
-def set_raw_score(atoms, raw_score):
-    """Set the raw_score of an atoms object in the
-    atoms.info['key_value_pairs'] dictionary.
-
-    Parameters
-    ----------
-    atoms : Atoms object
-        The atoms object that corresponds to this raw_score
-    raw_score : float or int
-        Independent calculation of how fit the candidate is.
-    """
-    if 'key_value_pairs' not in atoms.info:
-        atoms.info['key_value_pairs'] = {}
-    atoms.info['key_value_pairs']['raw_score'] = raw_score
+    return getattr(ase_ga, name)
 
 
 def get_raw_score(atoms):
