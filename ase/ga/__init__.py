2446f5bfd190e5c4f4e1cda0075f32cafed8a750
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-11-24 15:20:06 +0100

    general mechanism for replacing ase.ga imports

--- ase/ga/__init__.py
+++ ase/ga/__init__.py
@@ -1,4 +1,16 @@
-def __getattr__(name):
-    import ase_ga
+def deprecated_ga_get(oldmodulename, modulename):
+    def __getattr__(attrname):
+        import importlib
+        try:
+            module = importlib.import_module(modulename)
+        except ImportError as err:
+            raise ImportError(
+                f'Cannot import {modulename}.  '
+                'The ase.ga code has moved to a separate project, ase_ga: '
+                'https://github.com/dtu-energy/ase-ga .'
+                'Please install ase_ga, e.g., pip install ase_ga.') from err
+        return getattr(module, attrname)
+    return __getattr__
 
-    return getattr(ase_ga, name)
+
+__getattr__ = deprecated_ga_get(__name__, 'ase_ga')
