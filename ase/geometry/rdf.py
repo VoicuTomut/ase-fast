6234fbf382b88b58f6ffa5114fc5b4cac6be7721
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2025-12-20 06:22:44 +0100

    Fix and improve efficiency

--- ase/geometry/rdf.py
+++ ase/geometry/rdf.py
@@ -67,7 +67,6 @@ def get_rdf(atoms: Atoms, rmax: float, nbins: int,
     check_cell_and_r_max(atoms, rmax)
 
     natoms = len(atoms)
-    rdf = np.zeros(nbins + 1)
     dr = float(rmax / nbins)
 
     if elements is None:
@@ -75,41 +74,37 @@ def get_rdf(atoms: Atoms, rmax: float, nbins: int,
     else:
         i_indices = np.where(atoms.numbers == elements[0])[0]
 
-    phi = len(i_indices) / vol
-    norm = 2.0 * math.pi * dr * phi * natoms
-
+    rho = natoms / vol
+    norm = 4.0 * math.pi * dr * rho * len(i_indices)
+    rdf = np.zeros(nbins + 1)
     if distance_matrix is None:
-        nl = NeighborList(np.ones(natoms) * rmax * 0.5)
+        nl = NeighborList(np.ones(natoms) * rmax * 0.5, bothways=True)
         nl.update(atoms)
 
         for i in i_indices:
-            j_indices, _ = nl.get_neighbors(i)
+            j_indices, offsets = nl.get_neighbors(i)
             if elements is not None:
-                j_indices = [
-                        j for j in j_indices if atoms.numbers[j] == elements[1]
-                ]
+                mask = atoms.numbers[j_indices] == elements[1]
+                j_indices = j_indices[mask]
+                offsets = offsets[mask]
 
-            if not len(j_indices):
+            if np.count_nonzero(j_indices) == 0:
                 continue
 
-            distances = atoms.get_distances(i, j_indices, mic=True)
+            ps = atoms.positions
+            d = ps[j_indices] + offsets @ atoms.cell - ps[i]
+            distances = np.sqrt(np.add.reduce(d**2, axis=1))
+
             indices = np.asarray(np.ceil(distances / dr), dtype=int)
-            for index in range(nbins + 1):
-                rdf[index] += np.count_nonzero(indices == index)
+            rdf += np.bincount(indices, minlength=nbins + 1)[:nbins + 1]
     else:
         indices = np.asarray(np.ceil(distance_matrix / dr), dtype=int)
         if elements is None:
-            indices_triu = np.triu(indices)
-            for index in range(nbins + 1):
-                rdf[index] = np.count_nonzero(indices_triu == index)
-
+            x = indices.ravel()
         else:
-            norm *= 2
-            for i in i_indices:
-                for j in np.where(atoms.numbers == elements[1])[0]:
-                    index = indices[i, j]
-                    if index <= nbins:
-                        rdf[index] += 1
+            j_indices = np.where(atoms.numbers == elements[1])[0]
+            x = indices[i_indices][:, j_indices].ravel()
+        rdf = np.bincount(x, minlength=nbins + 1)[:nbins + 1].astype(float)
 
     rr = np.arange(dr / 2, rmax, dr)
     rdf[1:] /= norm * (rr * rr + (dr * dr / 12))
