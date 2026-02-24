d2611c6d117969278b138d5e6e9e66ddf6527269
Rasmus Niemann <s204029@dtu.dk>
2026-02-24 15:00:12 +0100

    added plot_atoms() and tight_layout for the periodic table plot

--- examples/gallery/ptable.py
+++ examples/gallery/ptable.py
@@ -1,8 +1,22 @@
-# creates: ptable.png
+"""
+
+Periodic Table
+==============
+
+
+"""
+
+
 from ase.utils.ptable import ptable
+from ase.visualize.plot import plot_atoms
+import matplotlib.pyplot as plt
 
 atoms = ptable()
 atoms.write('ptable.png')
+
+fig, ax = plt.subplots()
+ax = plot_atoms(atoms)
+fig.tight_layout()
 
 # Calling "povray ptable_pov.ini" will render it with povray.
 # atoms.write('ptable_pov.pov')
