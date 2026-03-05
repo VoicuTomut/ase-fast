cb00a4ed3f37dcaf701b28326147fb7a6b3c6ee2
SebastianWellig <sebastian.wellig@gmail.com>
2026-03-05 17:42:50 +0100

    Move helper functions to new constraint base file

--- ase/constraints/constraint.py
+++ ase/constraints/constraint.py
@@ -5,6 +5,49 @@ import numpy as np
 from ase import Atoms
 
 
+def slice2enlist(s, n):
+    """Convert a slice object into a list of (new, old) tuples."""
+    if isinstance(s, slice):
+        return enumerate(range(*s.indices(n)))
+    return enumerate(s)
+
+
+def ints2string(x, threshold=None):
+    """Convert ndarray of ints to string."""
+    if threshold is None or len(x) <= threshold:
+        return str(x.tolist())
+    return str(x[:threshold].tolist())[:-1] + ', ...]'
+
+
+def constrained_indices(atoms, only_include=None):
+    """Returns a list of indices for the atoms that are constrained
+    by a constraint that is applied.  By setting only_include to a
+    specific type of constraint you can make it only look for that
+    given constraint.
+    """
+    indices = []
+    for constraint in atoms.constraints:
+        if only_include is not None:
+            if not isinstance(constraint, only_include):
+                continue
+        indices.extend(np.array(constraint.get_indices()))
+    return np.array(np.unique(indices))
+
+
+def _normalize(direction):
+    if np.shape(direction) != (3,):
+        raise ValueError("len(direction) is {len(direction)}. Has to be 3")
+
+    direction = np.asarray(direction) / np.linalg.norm(direction)
+    return direction
+
+
+def _projection(vectors, direction):
+    dotprods = vectors @ direction
+    projection = direction[None, :] * dotprods[:, None]
+    return projection
+
+
 class FixConstraint:
     """Base class for classes that fix one or more atoms in some way."""
 
