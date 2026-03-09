92949c8d2ec11048259addb6f49ca88b61d85575
SebastianWellig <sebastian.wellig@gmail.com>
2026-03-09 12:08:56 +0100

    Rename constraints modules pep8-style

--- ase/constraints/__init__.py
+++ ase/constraints/__init__.py
@@ -7,26 +7,26 @@ from ase.constraints.constraint import (
     FixConstraint,
     constrained_indices,
 )
-from ase.constraints.externalforce import ExternalForce
-from ase.constraints.fixatoms import FixAtoms
-from ase.constraints.fixbondlengths import FixBondLength, FixBondLengths
-from ase.constraints.fixcartesian import FixCartesian
-from ase.constraints.fixcom import FixCom, FixSubsetCom
-from ase.constraints.fixedline import FixedLine
-from ase.constraints.fixedmode import FixedMode
-from ase.constraints.fixedplane import FixedPlane
-from ase.constraints.fixinternals import FixInternals
-from ase.constraints.fixlineartriatomic import FixLinearTriatomic
-from ase.constraints.fixparametricrelations import (
+from ase.constraints.external_force import ExternalForce
+from ase.constraints.fix_atoms import FixAtoms
+from ase.constraints.fix_bond_lengths import FixBondLength, FixBondLengths
+from ase.constraints.fix_cartesian import FixCartesian
+from ase.constraints.fix_com import FixCom, FixSubsetCom
+from ase.constraints.fix_internals import FixInternals
+from ase.constraints.fix_linear_triatomic import FixLinearTriatomic
+from ase.constraints.fix_parametric_relations import (
     FixCartesianParametricRelations,
     FixParametricRelations,
     FixScaledParametricRelations,
 )
-from ase.constraints.fixscaled import FixScaled
-from ase.constraints.fixsymmetry import FixSymmetry
+from ase.constraints.fix_scaled import FixScaled
+from ase.constraints.fix_symmetry import FixSymmetry
+from ase.constraints.fixed_line import FixedLine
+from ase.constraints.fixed_mode import FixedMode
+from ase.constraints.fixed_plane import FixedPlane
 from ase.constraints.hookean import Hookean
-from ase.constraints.mirrorforce import MirrorForce
-from ase.constraints.mirrortorque import MirrorTorque
+from ase.constraints.mirror_force import MirrorForce
+from ase.constraints.mirror_torque import MirrorTorque
 
 __all__ = [
     'FixCartesian',
