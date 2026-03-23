a264e28b3b6d66363397ae7987f0f2cb1f979e2b
Alireza Shabani <alsha@dtu.dk>
2026-03-23 13:46:06 +0100

    change a to atoms

--- examples/gallery/bs.py
+++ examples/gallery/bs.py
@@ -10,8 +10,8 @@ Band-structure
 from ase.build import bulk
 from ase.calculators.test import FreeElectrons
 
-a = bulk('Cu')
-a.calc = FreeElectrons(nvalence=1, kpts={'path': 'GXWLGK', 'npoints': 200})
-a.get_potential_energy()
-bs = a.calc.band_structure()
+atoms = bulk('Cu')
+atoms.calc = FreeElectrons(nvalence=1, kpts={'path': 'GXWLGK', 'npoints': 200})
+atoms.get_potential_energy()
+bs = atoms.calc.band_structure()
 bs.plot(emin=0, emax=20, filename='cu.png')
