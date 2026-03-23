2e695830b6c782561978aa87e4d065f622337808
elisabeth.keller <elisabeth.keller@gitlab.com>
2026-03-23 11:25:34 +0100

    Remove box and x and y axis of matplotlib plot to the left of the periodic table image

--- examples/gallery/ptable.py
+++ examples/gallery/ptable.py
@@ -15,9 +15,9 @@ atoms = ptable()
 atoms.write('ptable.png')
 
 fig, ax = plt.subplots()
-ax = plot_atoms(atoms)
+
+plot_atoms(atoms, ax=ax)
 ax.set_axis_off()
-fig.tight_layout()
 
 # Calling "povray ptable_pov.ini" will render it with povray.
 # atoms.write('ptable_pov.pov')
