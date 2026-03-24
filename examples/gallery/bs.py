bfae6f858dddefdbbebf482bafd85cc33434cc5b
Alireza Shabani <alsha@dtu.dk>
2026-03-24 10:39:25 +0100

    modified bandpath

--- examples/gallery/bs.py
+++ examples/gallery/bs.py
@@ -6,12 +6,12 @@ Band-structure
 
 """
 
-# creates: cu.png
 from ase.build import bulk
 from ase.calculators.test import FreeElectrons
 
 atoms = bulk('Cu')
-atoms.calc = FreeElectrons(nvalence=1, kpts={'path': 'GXWLGK', 'npoints': 200})
+band_path = atoms.cell.bandpath('GXWLGK', npoints=200)
+atoms.calc = FreeElectrons(nvalence=1, kpts=band_path)
 atoms.get_potential_energy()
 bs = atoms.calc.band_structure()
 bs.plot(emin=0, emax=20, filename='cu.png')
