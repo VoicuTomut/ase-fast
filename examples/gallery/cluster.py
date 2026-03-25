5fe3d6832e23dd9404eb95f05f1cae60e93669e5
Silvan Kretschmer <silkr@dtu.dk>
2026-03-25 12:43:56 +0100

    ruff

--- examples/gallery/cluster.py
+++ examples/gallery/cluster.py
@@ -6,7 +6,6 @@ Nanoparticle
 """
 
 from ase.cluster.cubic import FaceCenteredCubic
-from ase.visualize.plot import plot_atoms
 from ase.io import write
 
 surfaces = [(1, 0, 0), (1, 1, 0), (1, 1, 1)]
@@ -16,12 +15,8 @@ culayer = FaceCenteredCubic('Cu', surfaces, layers, latticeconstant=lc)
 culayer.rotate(6, 'x', rotate_cell=True)
 culayer.rotate(2, 'y', rotate_cell=True)
 
-# render with povray
 renderer = write('layer.Cu.pov', culayer)
 renderer.render()
-
-# povray alternative from ase.visualize
-# plot_atoms(culayer)
 
 surfaces = [(1, 0, 0), (1, 1, 1), (1, -1, 1)]
 layers = [6, 5, -1]
@@ -29,9 +24,5 @@ trunc = FaceCenteredCubic('Cu', surfaces, layers)
 trunc.rotate(6, 'x', rotate_cell=True)
 trunc.rotate(2, 'y', rotate_cell=True)
 
-# render with povray
 renderer = write('trunc.Cu.pov', trunc)
 renderer.render()
-
-# povray alternative from ase.visualize
-# plot_atoms(culayer)
