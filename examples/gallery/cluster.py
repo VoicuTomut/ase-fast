272234d18b7f7bbc1e4084e370dc4305afc96184
Silvan Kretschmer <silkr@dtu.dk>
2026-03-25 11:24:24 +0100

    Used image scraper for nanoparticle image

--- examples/gallery/cluster.py
+++ examples/gallery/cluster.py
@@ -7,6 +7,7 @@ Nanoparticle
 
 from ase.cluster.cubic import FaceCenteredCubic
 from ase.visualize.plot import plot_atoms
+from ase.io import write
 
 surfaces = [(1, 0, 0), (1, 1, 0), (1, 1, 1)]
 layers = [6, 9, 5]
@@ -14,11 +15,23 @@ lc = 3.61000
 culayer = FaceCenteredCubic('Cu', surfaces, layers, latticeconstant=lc)
 culayer.rotate(6, 'x', rotate_cell=True)
 culayer.rotate(2, 'y', rotate_cell=True)
-plot_atoms(culayer)
+
+# render with povray
+renderer = write('layer.Cu.pov', culayer)
+renderer.render()
+
+# povray alternative from ase.visualize
+# plot_atoms(culayer)
 
 surfaces = [(1, 0, 0), (1, 1, 1), (1, -1, 1)]
 layers = [6, 5, -1]
 trunc = FaceCenteredCubic('Cu', surfaces, layers)
 trunc.rotate(6, 'x', rotate_cell=True)
 trunc.rotate(2, 'y', rotate_cell=True)
-plot_atoms(trunc)
+
+# render with povray
+renderer = write('trunc.Cu.pov', trunc)
+renderer.render()
+
+# povray alternative from ase.visualize
+# plot_atoms(culayer)
